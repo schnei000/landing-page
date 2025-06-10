d22d0fe56e1ab667d11ea5cd2694f8d7be918f67
schnei000 <walkerstfleur007@gmail.com>
2025-06-09 20:44:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('searchBtn');
 const resultsDiv = document.getElementById('results');
 const loader = document.getElementById('loader');
 
-// API Unsplash
+
 const UNSPLASH_ACCESS_KEY = "YOUR_UNSPLASH_KEY"; 
 
 searchBtn.addEventListener('click', () => {
@@ -18,13 +18,13 @@ function searchDestination(query) {
   loader.style.display = "block";
   resultsDiv.innerHTML = `<div id="loader"> Chargement...</div>`;
 
-  // Récupérer image de Unsplash
+  
   fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${UNSPLASH_ACCESS_KEY}&orientation=landscape&per_page=1`)
     .then(res => res.json())
     .then(photoData => {
       const photo = photoData.results[0];
 
-      // Récupérer une description depuis Wikipedia
+      
       fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(query)}`)
         .then(res => res.json())
         .then(wikiData => {
